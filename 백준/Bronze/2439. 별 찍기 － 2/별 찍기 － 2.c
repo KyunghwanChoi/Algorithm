eebb2dc17761b38eb61ec583ce47752e25da53ba
ChoiColin <166238920+ChoiColin@users.noreply.github.com>
2024-09-13 15:35:36 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+    for(int i = 1; i <= n; i++){
+        for(int j = n; j > i; j--){
+            printf(" ");
+        }
+        
+        for(int k = 0; k < i; k++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
